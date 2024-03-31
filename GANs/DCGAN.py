24220f20b170f14673606b9b7b129840a672e492
nirbhay-design <sharma.59@iitj.ac.in>
2024-03-31 19:07:36 +0000

    training for DCGAN

--- GANs/DCGAN.py
+++ GANs/DCGAN.py
@@ -5,6 +5,13 @@ import torchvision
 import torchvision.transforms as transforms 
 import torch.optim as optim 
 
+def progress(current,total):
+    progress_percent = (current * 50 / total)
+    progress_percent_int = int(progress_percent)
+    print(f" |{chr(9608)* progress_percent_int}{' '*(50-progress_percent_int)}|{current}/{total}",end='\r')
+    if (current == total):
+        print()
+
 class Generator(nn.Module):
     def __init__(self, z_dim, channels=3):
         super().__init__()
@@ -95,10 +102,10 @@ def train(netG, netD, optG, optD, dataloader, lossfunction, epochs, device):
             optD.zero_grad()
 
             loss_disc_real = lossfunction(disc_real, real_labels)
-            loss_disc_real.backward()
-
             loss_disc_fake = lossfunction(disc_fake, fake_labels)
-            loss_disc_fake.backward()
+
+            loss_d = loss_disc_fake + loss_disc_real
+            loss_d.backward()
 
             optD.step()
 
@@ -112,14 +119,21 @@ def train(netG, netD, optG, optD, dataloader, lossfunction, epochs, device):
             optG.step()
 
             genloss += gen_loss / (len_data * data.shape[0])
-            discloss += (loss_disc_fake + loss_disc_real) / (len_data * data.shape[0])
+            discloss += loss_d / (len_data * data.shape[0])
+
+            progress(idx+1, len_data)
         
         print(f"epoch: [{epoch}/{epochs}], genloss: {genloss:.3f}, discloss: {discloss:.3f}")
 
     return netG, netD
 
 def dataset():
-    train_data = torchvision.datasets.MNIST('./', train=True, download=True, transform= transforms.Compose([transforms.Resize((64,64)), transforms.ToTensor()]))
+    transformations = transforms.Compose([
+        transforms.Resize((64,64)), 
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
+    train_data = torchvision.datasets.MNIST('../dataset', train=True, download=True, transform = transformations)
     train_loader = torch.utils.data.DataLoader(
         train_data,
         batch_size=32,
