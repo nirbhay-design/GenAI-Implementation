7749aff61fe8252f34095c7078660e5c539604c4
nirbhay-design <sharma.59@iitj.ac.in>
2024-03-31 23:58:16 +0530

    DCGAN

---
+++ GANs/DCGAN.py
@@ -0,0 +1,143 @@
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+import torchvision 
+import torchvision.transforms as transforms 
+import torch.optim as optim 
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels=3):
+        super().__init__()
+        self.gen = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, 1024, 4, 1, 0),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(128, channels, 4, 2, 1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        B, zdim = x.shape
+        x = x.reshape(B, zdim, 1, 1)
+        return self.gen(x)
+
+class Discriminator(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+
+        self.disc = nn.Sequential(
+            nn.Conv2d(channels, 128, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 256, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 512, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(512, 1024, kernel_size = 4, stride = 2, padding = 0),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(1024, 1, 4, 2, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.disc(x).flatten(1)
+    
+def essentials():
+    netG = Generator(100, 1)
+    netD = Discriminator(1) 
+    optG = optim.Adam(params = netG.parameters(), lr = 0.0002)
+    optD = optim.Adam(params = netD.parameters(), lr = 0.0002)
+    return netG, netD, optG, optD 
+
+def train(netG, netD, optG, optD, dataloader, lossfunction, epochs, device):
+    netG.train()
+    netD.train() 
+
+    netG = netG.to(device)
+    netD = netD.to(device)
+    len_data = len(dataloader)
+
+    for epoch in range(epochs):
+        genloss = 0
+        discloss = 0
+        for idx, (data, _) in enumerate(dataloader):
+            data = data.to(device)
+
+            # discriminator loss min -(log(D(x)) + log(1 - D(G(z))))
+            z = torch.randn(data.shape[0], 100)
+            fake_data = netG(z)
+            disc_real = netD(data)
+            disc_fake = netD(fake_data.detach())
+
+            real_labels = torch.ones_like(disc_real)
+            fake_labels = torch.zeros_like(disc_fake)
+
+            optD.zero_grad()
+
+            loss_disc_real = lossfunction(disc_real, real_labels)
+            loss_disc_real.backward()
+
+            loss_disc_fake = lossfunction(disc_fake, fake_labels)
+            loss_disc_fake.backward()
+
+            optD.step()
+
+            # generator loss min log(1 - D(G(z))) -> max log(D(G(z))) -> min -log(D(G(z)))
+            dis_gen_fake = netD(fake_data)
+            dis_gen_real_labels = torch.ones_like(dis_gen_fake)
+            gen_loss = lossfunction(dis_gen_fake, dis_gen_real_labels)
+
+            optG.zero_grad()
+            gen_loss.backward()
+            optG.step()
+
+            genloss += gen_loss / (len_data * data.shape[0])
+            discloss += (loss_disc_fake + loss_disc_real) / (len_data * data.shape[0])
+        
+        print(f"epoch: [{epoch}/{epochs}], genloss: {genloss:.3f}, discloss: {discloss:.3f}")
+
+    return netG, netD
+
+def dataset():
+    train_data = torchvision.datasets.MNIST('./', train=True, download=True, transform=transforms.ToTensor)
+    train_loader = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=32,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=4
+    )
+
+    return train_loader
+
+def main():
+    netG, netD, optG, optD = essentials()
+    dataloader = dataset()
+    device = torch.device("cuda:0")
+    lossfunction = nn.BCELoss()
+    epochs = 20
+    train(netG, netD, optG, optD, dataloader, lossfunction, epochs, device)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 - [ ] [Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network](https://openaccess.thecvf.com/content_cvpr_2017/papers/Ledig_Photo-Realistic_Single_Image_CVPR_2017_paper.pdf)
 - [ ] [ESRGAN: Enhanced Super-Resolution Generative Adversarial Networks](https://openaccess.thecvf.com/content_ECCVW_2018/papers/11133/Wang_ESRGAN_Enhanced_Super-Resolution_Generative_Adversarial_Networks_ECCVW_2018_paper.pdf)
 - [ ] [Unpaired Image-to-Image Translation using Cycle-Consistent Adversarial Networks](https://openaccess.thecvf.com/content_ICCV_2017/papers/Zhu_Unpaired_Image-To-Image_Translation_ICCV_2017_paper.pdf)
+- [ ] [UNSUPERVISED REPRESENTATION LEARNING WITH DEEP CONVOLUTIONAL GENERATIVE ADVERSARIAL NETWORKS](https://arxiv.org/pdf/1511.06434.pdf)
+
+# GAN Hacks
+
+- No sparse gradients (no maxpool, ReLU)
+- Slow learning rate 
+- use batch normalization, LeakyReLU, dropout
+- Implement discriminator loss function in two goes 
