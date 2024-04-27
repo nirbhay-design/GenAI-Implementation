3b67d6b360b2a66d21ff9ae6c763d683730e3121
nirbhay-design <sharma.59@iitj.ac.in>
2024-04-28 00:47:38 +0530

    starting implementation of SRGAN

---
+++ GANs/SRGAN.py
@@ -0,0 +1,16 @@
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+import torchvision 
+import torchvision.transforms as transforms 
+import torch.optim as optim 
+
+class ConvResBLock(nn.Module):
+    def __init__(self):
+        super().__init__(self)
+    
+    def forward(self, x):
+        pass 
+
+if __name__ == "__main__":
+    pass 
