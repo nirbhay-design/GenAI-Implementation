bf41ef8b3ae576eb0c46471b8dab5dfd047fd3c3
nirbhay-design <sharma.59@iitj.ac.in>
2024-07-25 19:20:06 +0530

    adding vit gan

---
+++ GANs/ViTGAN.py
@@ -0,0 +1,10 @@
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+import torch.optim as optim 
+import torchvision 
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np 
+
+
