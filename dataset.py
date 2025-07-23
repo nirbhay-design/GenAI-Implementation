3115d5c22934f21bafb4ef21eb866110d04060bd
nirbhay-design <sharma.59@iitj.ac.in>
2025-07-23 22:03:52 +0530

    creating the setup for repo

---
+++ dataset.py
@@ -0,0 +1,13 @@
+import torch 
+import os 
+
+class AFHQ(object):
+    def __init__(self, data_dir):
+        self.datadir = data_dir 
+
+    def __len__(self):
+        pass 
+
+    def __getitem__(self, idx):
+        pass 
+        
---
+++ ganloss.py
@@ -0,0 +1,25 @@
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+
+class MinMaxLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass 
+
+class HingeGanLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass 
+
+class LSGanLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass 
+
